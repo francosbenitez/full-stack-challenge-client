0ae2d2aa660e17fba6367e20ab8646987c956bef
francosbenitez <francosbenitez@gmail.com>
2021-09-26 20:31:22 -0300

    feat: add navbar & footer

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <header className="px-10 md:px-32 flex justify-between flex-wrap items-center py-2 bg-opacity-70 top-0 fixed w-screen z-10 bg-gray-50 border-b dark:border-darkergrey border-gray-200 dark:bg-black">
+            <div className="flex flex-row">
+                <h1 className="text-3xl font-bold">
+                    <a href="/" className="logo">
+                        Budget App
+                    </a>
+                </h1>
+            </div>
+            <label htmlFor="menu-toggle" className="cursor-pointer lg:hidden block p-4">
+                <svg
+                className="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                >
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                </svg>
+            </label>
+            <input className="hidden" type="checkbox" id="menu-toggle" />
+            <div className="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
+                <nav>
+                    <ul className="lg:flex-1 pt-4 justify-center text-center lg:pt-0">
+                        <li className="grid lg:block gap-5">
+                            <span>
+                                <a href="/#"
+                                    className="text-sm font-medium lg:m-4 lg:inline"
+                                >
+                                Title 1
+                                </a>
+                            </span>
+                            <span className="mt:4 pb-5">
+                                <a href="/#"
+                                    className="text-sm font-medium lg:m-4 lg:inline"
+                                >
+                                Title 2
+                                </a>
+                            </span>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Navbar;
---
+++ src/views/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "../components/layout/Navbar"
+import Footer from "../components/layout/Footer"
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <Footer />
+        </>
+    );
+};
+
+export default Home;
