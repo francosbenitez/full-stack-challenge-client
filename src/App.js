a02b48736e052ee66c181793065f00168a1557a6
francosbenitez <francosbenitez@gmail.com>
2021-09-26 12:42:37 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="bg-primary text-primary md:overflow-hidden">
+        <div className="px-4 py-20 md:py-4">
+          <div className="md:max-w-6xl md:mx-auto">
+            <div className="md:flex md:flex-wrap">
+              <div className="md:w-1/2 text-center md:text-left md:pt-16">
+                <h1 className="font-bold text-2xl text-bold md:text-5xl leading-tight mb-4">"Testing Tailwind"</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
