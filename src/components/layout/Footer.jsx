f1d880ae0c1da57a7313a77abe178a44dbd67321
francosbenitez <francosbenitez@gmail.com>
2021-10-08 20:19:08 -0300

    chore: delete Balance component | style: modify footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="fixed bottom-0 left-0 w-full text-center border-t border-grey p-6 bg-white">
             <div className="flex justify-center w-11/12 mx-auto mt-1 items-center text-sm">
-            © {new Date().getFullYear()} |
+            © {new Date().getFullYear()} | Built with ☕️ by 
                 <a href="https://github.com/francosbenitez/" className="ml-1">
                 Franco Sebastián Benítez
                 </a>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from "../components/layout/Navbar"
 import Footer from "../components/layout/Footer"
 import AddButton from "../components/operations/AddButton"
-import Balance from "../components/operations/Balance"
 import List from "../components/operations/List"
 
 const Home = () => {
@@ -10,7 +9,6 @@ const Home = () => {
         <>
             <Navbar />
             <AddButton />
-            <Balance />
             <List />
             <Footer />
         </>
