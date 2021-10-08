82867e50194ad855d12134d3a9a338c67b005309
francosbenitez <francosbenitez@gmail.com>
2021-10-08 20:12:39 -0300

    feat: added balance calculation

--- src/components/operations/Balance.jsx
+++ src/components/operations/Balance.jsx
@@ -3,12 +3,7 @@ import React from "react";
 const Balance = () => {
     return (
         <>
-            <div className="flex flex-col items-center justify-center px-5 m-auto text-center lg:w-2/3">
-                <h1 className="my-5 font-bold">Balance</h1>
-                <span className="w-5/6 text-center lg:text-lg text-md">
-                $
-                </span>
-            </div>
+            
         </>
     );
 };
--- src/components/operations/List.jsx
+++ src/components/operations/List.jsx
@@ -33,41 +33,54 @@ const List = () => {
         .catch(error => console.log(error))
         setListUpdated(true)
     }
+    const Balance = operations.reduce((acc, { amount, type }) => {
+        if (type === 'Income') {
+          return acc + Number(amount);
+        } else {
+          return acc - Number(amount);
+        }
+    }, 0);
     return (
-        <div className="p-10">
-            <h1 className="mb-5 text-center font-bold">Last 10 Records</h1>
-            <div className="overflow-x-auto">
-                <table className="w-full">
-                    <thead>
-                        <tr className="bg-white">
-                            <th className="p-1 border border-text">#</th>
-                            <th className="p-1 border border-text">Concept</th>
-                            <th className="p-1 border border-text">Amount</th>
-                            <th className="p-1 border border-text">Date</th>
-                            <th className="p-1 border border-text">Category</th>
-                            <th className="p-1 border border-text">Type</th>
-                            <th className="p-1 border border-text">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody className="text-center">
-                            {operations.map(operation => 
-                                <tr className="bg-white" key={operation.operation_id}>
-                                    <td className="p-1 border border-text" tableheaddata="#">{operation.operation_id}</td>
-                                    <td className="p-1 border border-text" tableheaddata="Concept">{operation.concept}</td>
-                                    <td className="p-1 border border-text" tableheaddata="Amount">{new Intl.NumberFormat("en-EN").format(operation.amount)}</td>
-                                    <td className="p-1 border border-text" tableheaddata="Date">{moment.utc(operation.date).format("DD/MM/YY")}</td>
-                                    <td className="p-1 border border-text" tableheaddata="Category">{operation.category}</td>
-                                    <td className="p-1 border border-text" tableheaddata="Type">{operation.type}</td>
-                                    <td className="p-1 border border-text">
-                                        <Link to={`/update/${operation.operation_id}`} style={{backgroundImage: 'none'}}>
-                                            <button className="mr-1 px-3 bg-green-400"><FontAwesomeIcon icon={faEdit}/></button>
-                                        </Link>
-                                        <button onClick={() => handleDelete(operation.operation_id)}  className="ml-1 px-3 bg-red-400"><FontAwesomeIcon icon={faTrashAlt}/></button>
-                                    </td>
-                                </tr>
-                            )}
-                    </tbody>
-                </table>
+        <div className="flex flex-col items-center justify-center px-5 m-auto text-center lg:w-2/3">
+            <h1 className="my-5 font-bold">Balance</h1>
+            <span className="w-5/6 text-center lg:text-lg text-md">
+                ${Balance}
+            </span>
+            <div className="p-10">
+                <h1 className="mb-5 text-center font-bold">Last 10 Records</h1>
+                <div className="overflow-x-auto">
+                    <table className="w-full">
+                        <thead>
+                            <tr className="bg-white">
+                                <th className="p-1 border border-text">#</th>
+                                <th className="p-1 border border-text">Concept</th>
+                                <th className="p-1 border border-text">Amount</th>
+                                <th className="p-1 border border-text">Date</th>
+                                <th className="p-1 border border-text">Category</th>
+                                <th className="p-1 border border-text">Type</th>
+                                <th className="p-1 border border-text">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody className="text-center">
+                                {operations.map(operation => 
+                                    <tr className="bg-white" key={operation.operation_id}>
+                                        <td className="p-1 border border-text" tableheaddata="#">{operation.operation_id}</td>
+                                        <td className="p-1 border border-text" tableheaddata="Concept">{operation.concept}</td>
+                                        <td className="p-1 border border-text" tableheaddata="Amount">{new Intl.NumberFormat("en-EN").format(operation.amount)}</td>
+                                        <td className="p-1 border border-text" tableheaddata="Date">{moment.utc(operation.date).format("DD/MM/YY")}</td>
+                                        <td className="p-1 border border-text" tableheaddata="Category">{operation.category}</td>
+                                        <td className="p-1 border border-text" tableheaddata="Type">{operation.type}</td>
+                                        <td className="p-1 border border-text">
+                                            <Link to={`/update/${operation.operation_id}`} style={{backgroundImage: 'none'}}>
+                                                <button className="mr-1 px-3 bg-green-400"><FontAwesomeIcon icon={faEdit}/></button>
+                                            </Link>
+                                            <button onClick={() => handleDelete(operation.operation_id)}  className="ml-1 px-3 bg-red-400"><FontAwesomeIcon icon={faTrashAlt}/></button>
+                                        </td>
+                                    </tr>
+                                )}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
