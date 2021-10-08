f1aa105af14d317b41ebec261d9b61ed6a6d3d34
francosbenitez <francosbenitez@gmail.com>
2021-10-08 09:10:42 -0300

    feat: improved update operation

--- src/views/Update.jsx
+++ src/views/Update.jsx
@@ -1,7 +1,13 @@
 import React, {useState} from 'react';
-import Form from "../components/operations/Form"
+//import Form from "../components/operations/Form"
+import { useHistory, Link } from 'react-router-dom';
 
 const Update = ({operations, setListUpdated}) => {
+    //const location = useLocation()
+    //console.log(location.pathname)
+    const history = useHistory()
+    //console.log(history.location.pathname)
+
     const [operation, setOperation] = useState({
         concept: "", 
         amount: "",
@@ -10,13 +16,20 @@ const Update = ({operations, setListUpdated}) => {
         type: "",
         user_id: ""
     })
-    const handleUpdate = id => {
+    const handleChange = e => {
+        setOperation({
+            ...operation,
+            [e.target.name]: e.target.value
+        })
+    }
+    const {concept, amount, date, category, type } = operation
+    const handleUpdate = () => {
         const requestInit = {
             method: "PUT",
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(operation)
         }
-        fetch('http://localhost:5000/operation/update/' + id, requestInit)
+        fetch(`http://localhost:5000/operation${history.location.pathname}`, requestInit)
         .then(res => res.text())
         .then(res => console.log(res))
         setOperation({
@@ -32,7 +45,74 @@ const Update = ({operations, setListUpdated}) => {
     return (
         <div>
             <p>Update</p>
-            <Form operation={operation} setOperation={setOperation} />
+            <div className="w-full flex justify-center">
+                <form onSubmit={handleUpdate} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                        <div className="mb-4">
+                            <label htmlFor="concept" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Concept</label>
+                            <input value={concept} name="concept" onChange={handleChange} type="text" id="concept" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Concept" />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="amount" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                Amount
+                            </label>
+                            <input value={amount} name="amount" onChange={handleChange} type="number" id="amount" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="1000" />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="date" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                Date
+                            </label>
+                            <input value={date} name="date" onChange={handleChange} type="date" id="date" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="category" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                Category
+                            </label>
+                            <div className="relative">
+                                <select value={category} name="category" onChange={handleChange} type="text" id="category" className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                    <option selected hidden>Choose here</option>
+                                    <option>Food</option>
+                                    <option>Health</option>
+                                </select>
+                                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="type" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                Type
+                            </label>
+                            <div className="relative">
+                                <select value={type} name="type" onChange={handleChange} type="type" id="type" className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                    <option selected hidden>Choose here</option>
+                                    <option>Income</option>
+                                    <option>Expense</option>
+                                </select>
+                                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                                Update
+                            </button>
+                            <Link to="/" style={{backgroundImage: 'none'}}>
+                                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                                    Cancel
+                                </button>
+                            </Link>
+                        </div>
+                        {/*<div className="flex flex-col justify-center items-center">
+                            <button
+                                type="submit"
+                                className="lg:mx-4 bg-background border-2 border-primary border-opacity-100 hover:bg-primary text-primary hover:text-background font-bold py-2 px-4 rounded"
+                            >
+                                Create
+                            </button>
+                        </div>*/}
+                </form>
+            </div>
         </div>
     );
 };
