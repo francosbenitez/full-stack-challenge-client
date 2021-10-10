b8ae6d63e3c178b9236746d92db4f374e6ff9048
francosbenitez <francosbenitez@gmail.com>
2021-10-09 22:45:13 -0300

    feat: implementing UserContext

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import Update from "./pages/Update"
 import Register from "./pages/auth/Register"
 import Login from "./pages/auth/Login"
 import Footer from "./components/layout/Footer"
+import { UserContext } from "./UserContext"
+import React, { useState, useMemo } from "react";
 
 function App() {
   document.title = "Budget App | Franco Sebastián Benítez";
+  const [user, setUser] = useState(null);
+  const value = useMemo(() => ({user, setUser}), [user, setUser])
   return (
     <>
       <Router>
-        <Route path="/" exact={true} component={Register} />
-        <Route path="/login" exact={true} component={Login} />
-        <Route path="/home" exact={true} component={Home} />
-        <Route path="/add" exact={true} component={Add} />
-        <Route path="/update/:id" exact={true} component={Update} />
+        <UserContext.Provider value={value}>
+          <Route path="/" exact={true} component={Register} />
+          <Route path="/login" exact={true} component={Login} />
+          <Route path="/home" exact={true} component={Home} />
+          <Route path="/add" exact={true} component={Add} />
+          <Route path="/update/:id" exact={true} component={Update} />
+        </UserContext.Provider>
       </Router>
       <Footer />
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Navbar from "../components/layout/Navbar"
 import Footer from "../components/layout/Footer"
 import AddButton from "../components/operations/AddButton"
 import List from "../components/operations/List"
+import { UserContext } from "../UserContext"
+import { login } from "../utils/login"
 
 const Home = () => {
+    const { user, setUser } = useContext(UserContext);
     return (
         <>
             <Navbar />
             <AddButton />
+            <pre>{JSON.stringify(user, null, 2)}</pre>
+            {user ? (
+                <button
+                onClick={() => {
+                    setUser(null);
+                }}
+                >
+                logout
+                </button>
+            ) : (
+                <button
+                onClick={async () => {
+                    const user = await login();
+                    setUser(user);
+                }}
+                >
+                login
+                </button>
+            )}
             <List />
             <Footer />
         </>
